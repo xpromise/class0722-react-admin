ad99c9ad2533927affb2ab80642ca144059e1011
xiongjian <webjsforyou@163.com>
2019-11-19 15:27:24 +0800

    完成Login静态组件

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import routes from "./config/routes";
 
+import "./index.less";
+
 export default class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          {routes.map(route => {
-            return <Route {...route} key={route.path} />;
+          {routes.map((route, index) => {
+            return <Route {...route} key={index} />;
           })}
         </Switch>
       </Router>
--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,7 +1,41 @@
 import React, { Component } from "react";
+import { Form, Input, Button, Icon } from "antd";
+import logo from "./logo.png";
+import "./index.less";
+
+const { Item } = Form;
 
 export default class Login extends Component {
   render() {
-    return <div>Login...</div>;
+    return (
+      <div className="login">
+        <header className="login-header">
+          <img src={logo} alt="logo" />
+          <h1>React项目: 后台管理系统</h1>
+        </header>
+        <section className="login-section">
+          <h3>用户登录</h3>
+          <Form>
+            <Item>
+              <Input
+                prefix={<Icon type="user" className="login-icon" />}
+                placeholder="用户名"
+              />
+            </Item>
+            <Item>
+              <Input
+                prefix={<Icon type="lock" className="login-icon" />}
+                placeholder="密码"
+              />
+            </Item>
+            <Item>
+              <Button type="primary" className="login-btn">
+                登录
+              </Button>
+            </Item>
+          </Form>
+        </section>
+      </div>
+    );
   }
 }
