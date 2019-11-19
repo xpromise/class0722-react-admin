acd91be08cf8b25888b8f77a240662e987d4f8f7
xiongjian <webjsforyou@163.com>
2019-11-19 16:32:06 +0800

    完成Login组件表单校验工鞥呢

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,25 @@
  const {
    override,
    fixBabelImports,
-   addLessLoader
+   addLessLoader,
+   addDecoratorsLegacy
  } = require('customize-cra');
 
  module.exports = override(
+   // 按需加载
    fixBabelImports('import', {
      libraryName: 'antd',
      libraryDirectory: 'es',
      style: true,
    }),
+   // 自定义主题
    addLessLoader({
      javascriptEnabled: true,
      modifyVars: {
        '@primary-color': '#1DA57A'
      },
    }),
+   // 添加装饰器语法
+   // yarn add @babel/plugin-proposal-decorators --dev
+   addDecoratorsLegacy()
  );
--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -5,8 +5,37 @@ import "./index.less";
 
 const { Item } = Form;
 
-export default class Login extends Component {
+@Form.create()
+class Login extends Component {
+  validator = (rule, value, callback) => {
+    /*
+      rule 用来获取当前校验的是哪个表单/Input
+      value 当前表单项的值
+      callback 不管校验成功还是失败 必须调用的函数
+        callback() 代表校验成功
+        callback('xxx') 代表校验失败
+    */
+    const name = rule.field === "username" ? "用户名" : "密码";
+
+    if (!value) {
+      // 说明没有值
+      callback("请输入" + name);
+    } else if (value.length < 4) {
+      callback(name + "长度至少大于4位");
+    } else if (value.length > 13) {
+      callback(name + "长度不能大于13位");
+    } else if (!/\w/.test(value)) {
+      callback(name + "只能包含英文、数字和下划线");
+    } else {
+      // 注意callback必须调用
+      callback();
+    }
+  };
+
   render() {
+    // getFieldDecorator方法也是一个高阶组件用法
+    const { getFieldDecorator } = this.props.form;
+
     return (
       <div className="login">
         <header className="login-header">
@@ -14,19 +43,56 @@ export default class Login extends Component {
           <h1>React项目: 后台管理系统</h1>
         </header>
         <section className="login-section">
-          <h3>用户登录</h3>
           <Form>
+            <h3>用户登录</h3>
+
             <Item>
-              <Input
-                prefix={<Icon type="user" className="login-icon" />}
-                placeholder="用户名"
-              />
+              {getFieldDecorator("username", {
+                // 表单校验规则
+                rules: [
+                  // 一般适用于只有一条校验规则
+                  /* {
+                    required: true, // 必填项
+                    message: "请输入用户名" // 一旦表单校验失败提示的错误信息
+                  },
+                  {
+                    min: 4,
+                    message: "用户名长度至少大于4位"
+                  },
+                  {
+                    max: 13,
+                    message: "用户名长度不能大于13位"
+                  },
+                  {
+                    pattern: /\w/,
+                    message: "用户名只能包含英文、数字和下划线"
+                  } */
+                  {
+                    // 适用于多条校验规则，并可复用
+                    validator: this.validator
+                  }
+                ]
+              })(
+                <Input
+                  prefix={<Icon type="user" className="login-icon" />}
+                  placeholder="用户名"
+                />
+              )}
             </Item>
             <Item>
-              <Input
-                prefix={<Icon type="lock" className="login-icon" />}
-                placeholder="密码"
-              />
+              {getFieldDecorator("password", {
+                rules: [
+                  {
+                    validator: this.validator
+                  }
+                ]
+              })(
+                <Input
+                  prefix={<Icon type="lock" className="login-icon" />}
+                  type="password"
+                  placeholder="密码"
+                />
+              )}
             </Item>
             <Item>
               <Button type="primary" className="login-btn">
@@ -39,3 +105,8 @@ export default class Login extends Component {
     );
   }
 }
+
+// Form.create方法是一个高阶组件用法。 作用：给组件传递form属性
+// export default Form.create()(Login);
+
+export default Login;
